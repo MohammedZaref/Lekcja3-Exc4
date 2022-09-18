0cd9175ef1f9939998d3919205ee4bbb980368b4
Mohammed Elkady <melkady254@gmail.com>
2022-09-18 21:10:04 +0100

    moved int min to SecondsToMinutes

--- src/main/java/Future/Collars/SecondsToMinutes.java
+++ src/main/java/Future/Collars/SecondsToMinutes.java
@@ -3,7 +3,7 @@ package Future.Collars;
 public class SecondsToMinutes {
     static final int min = 60;
 
-    public static double secondsToMinutesConverter(double seconds) {
+    protected static double secondsToMinutesConverter(double seconds) {
         return seconds / min;
     }
 }

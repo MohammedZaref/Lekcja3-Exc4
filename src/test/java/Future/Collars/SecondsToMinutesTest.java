6c2528c31a98c382ef2117536897bbb207d604c9
Mohammed Elkady <melkady254@gmail.com>
2022-09-18 21:10:26 +0100

    tested SecondsToMinutesTest

--- src/test/java/Future/Collars/SecondsToMinutesTest.java
+++ src/test/java/Future/Collars/SecondsToMinutesTest.java
@@ -1,7 +1,15 @@
 package Future.Collars;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 class SecondsToMinutesTest {
+    @Test
+    void shouldConvertSecondsToMinutes() {
+        var secondsToMinutes = new SecondsToMinutes();
+        Assertions.assertEquals(4, SecondsToMinutes.secondsToMinutesConverter(240));
+
+    }
+
 
 }
